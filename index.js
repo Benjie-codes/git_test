1c4986ecac5aa52eab03fc5f0ccdcf9ad7bf37c7
Benjamin Samuel <me.benjhamine@gmail.com>
2023-11-17 12:46:23 +0000

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // document.getElementById("count_el").innerHTML=5
 
-let myAge = 23
-console.log(myAge)
+// let myAge = 23 console.log(myAge)
+
+// function increment() {
+
+// }
+
+function myLogger () {
+    console.log(42)
+}
+
+myLogger()
