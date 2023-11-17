aa30cd6308135a517f11272dcf5f6cd12201cbe9
Benjamin Samuel <me.benjhamine@gmail.com>
2023-11-17 13:48:25 +0000

    Add new js file and update

---
+++ excercise.js
@@ -0,0 +1,12 @@
+// let username = 'benji'
+
+// let message = 'You have three new notifications'
+// let messageToUser = username + ', ' + message+ "!"
+
+// console.log(messageToUser)
+
+let name = 'benji'
+let greeting = 'Hi, my name is '
+
+myGreeting = greeting + name
+console.log(myGreeting)
--- index.js
+++ index.js
@@ -6,8 +6,31 @@
 
 // }
 
-function myLogger () {
-    console.log(42)
+// Lap increment function
+// let lapsCompleted = 0
+
+// function totalLap() {
+//     lapsCompleted = lapsCompleted + 1
+//     console.log(lapsCompleted)
+// }
+
+// totalLap()
+// totalLap()
+// totalLap()
+
+let count_El = document.getElementById("count_el")
+
+let count = 0
+
+function increment(){
+    count = count + 1
+    count_El.innerText=count
 }
 
-myLogger()
+function save() {
+    console.log(count)
+}
+
+save()
+
+
